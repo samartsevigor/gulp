d012aae41fcfcb248a29e69087c5ac78c6066816
Shoppen <shoppen@gmail.com>
2021-02-04 12:52:46 +0400

    update plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-let {src, dest} = require('gulp')
-let gulp = require('gulp')
-let browsersync = require('browser-sync').create();
-let sass = require('gulp-sass')
-let autoprefixer = require('gulp-autoprefixer')
-let fileinclude = require('gulp-file-include')
-let imagemin = require('gulp-imagemin')
-let ttf2woff2 = require('gulp-ttf2woff2')
-let ttf2woff = require('gulp-ttf2woff')
-let gcmq = require('gulp-group-css-media-queries')
+let {src, dest} = require('gulp'),
+  gulp = require('gulp'),
+  browsersync = require('browser-sync').create(),
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  fileinclude = require('gulp-file-include'),
+  ttf2woff2 = require('gulp-ttf2woff2'),
+  ttf2woff = require('gulp-ttf2woff'),
+  gcmq = require('gulp-group-css-media-queries'),
+  cleanCSS = require('gulp-clean-css')
 
 function browserSync() {
   browsersync.init({
@@ -18,37 +18,39 @@ function browserSync() {
   })
 }
 
-function html () {
+function html() {
   return src(['./src/*.html', '!./src/_*.html'])
     .pipe(fileinclude())
     .pipe(dest('./dist/'))
     .pipe(browsersync.stream())
 }
 
-function css () {
-  return src('./src/scss/*.scss')
+function js() {
+  return src('./src/js/*.js')
+    .pipe(dest('./dist/js/'))
+    .pipe(browsersync.stream())
+}
+
+function css() {
+  return src(['./src/scss/*.scss', '!./src/scss/_*.scss'])
     .pipe(sass())
     .pipe(gcmq())
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(autoprefixer({
-      overrideBrowserlist:["last 5 version"],
+      overrideBrowserlist: ["last 5 version"],
       cascade: true
     }))
     .pipe(dest('./dist/css/'))
     .pipe(browsersync.stream())
 }
-function images () {
-  return src('./src/images/*.{png, jpg, gif, svg, webp}')
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{removeViewBox: false}],
-      interlaced: true,
-      optimizationLevel: 3
-    }))
+
+function images() {
+  return src('./src/images/*.{jpg, png, svg, gif, webp, ico}')
     .pipe(dest('./dist/images/'))
     .pipe(browsersync.stream())
 }
 
-function fonts () {
+function fonts() {
   src(['./src/fonts/*.ttf'])
     .pipe(ttf2woff())
     .pipe(gulp.dest('./dist/fonts/'))
@@ -57,16 +59,18 @@ function fonts () {
     .pipe(gulp.dest('./dist/fonts/'));
 }
 
-function watchFiles () {
+function watchFiles() {
   gulp.watch(['./src/*.html'], html)
   gulp.watch(['./src/scss/*.scss'], css)
   gulp.watch(['./src/images/*.{png, jpg, gif, svg, webp}'], images)
   gulp.watch(['./src/fonts/*'], fonts)
+  gulp.watch(['./src/js/*'], js)
 }
 
-let build = gulp.series(gulp.parallel(html,  css, images, fonts))
-let watch = gulp.parallel(build,watchFiles,browserSync)
+let build = gulp.series(gulp.parallel(html, css, images, fonts, js))
+let watch = gulp.parallel(build, watchFiles, browserSync)
 
+exports.js = js
 exports.images = images
 exports.html = html
 exports.watchFiles = watchFiles
